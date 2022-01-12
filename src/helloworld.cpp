93969bf030418857b299bdd7e022c98b7b0c27fe
user1687569 <user1687569@gmail.com>
2022-01-12 13:16:58 +0800

    optimize cpp file

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,3 +1,5 @@
+//Copyright (c) 2022 user1687569
+
 #include "wx/wxprec.h"
 #ifndef WX_PRECOMP
     #include "wx/wx.h"
@@ -6,29 +8,39 @@
 
 class MyApp : public wxApp
 {
-public:
-    virtual bool OnInit();
+    public:
+        virtual bool OnInit();
 };
+
+
 class MyFrame : public wxFrame
 {
-public:
-    MyFrame();
-private:
-    void OnHello(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
+    public:
+        MyFrame();
+    private:
+        void OnHello(wxCommandEvent& event);
+        void OnExit(wxCommandEvent& event);
+        void OnAbout(wxCommandEvent& event);
 };
+
+
 enum
 {
     ID_Hello = 1
 };
+
+
 wxIMPLEMENT_APP(MyApp);
+
+
 bool MyApp::OnInit()
 {
     MyFrame *frame = new MyFrame();
     frame->Show(true);
     return true;
 }
+
+
 MyFrame::MyFrame()
     : wxFrame(NULL, wxID_ANY, "Hello World")
 {
@@ -49,16 +61,20 @@ MyFrame::MyFrame()
     Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
 }
+
+
 void MyFrame::OnExit(wxCommandEvent& event)
 {
     Close(true);
 }
+
 
 void MyFrame::OnAbout(wxCommandEvent& event)
 {
     wxMessageBox("This is a wxWidgets Hello World example",
                  "About Hello World", wxOK | wxICON_INFORMATION);
 }
+
 
 void MyFrame::OnHello(wxCommandEvent& event)
 {
